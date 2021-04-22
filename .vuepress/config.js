2317377e7703c0ff0801222957db14bbbb95ea3b
zhangtao <sheseerzuo123>
2021-04-22 11:03:32 +0800

    Add doc

---
+++ .vuepress/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  title: '英雄榜',
+  description: '致敬我们最可爱的人',
+    base: '/',
+
+    evergreen: true,
+    editLinkText: '在 GitHub 上编辑此页',
+    port: 8081,
+    ga: 'G-K7BXQR7PS1',
+    themeConfig: {
+        repo: 'honourLove/honourLove.github.io',
+        editLinks: true,
+        editLinkText: '编辑此页面！',
+        nav: [
+            {text: '英雄榜', link: '/list/list'},
+              {text: '恶心公知', link: '/list/fuck-public-intellectual'}
+
+        ]
+
+    }
+}
---
+++ list/fuck-public-intellectual.md
@@ -0,0 +1,13 @@
+# 世界名画欣赏
+
+![tuao1.jpg](./img/fpi/tuao1.jpg)
+
+
+![tuao2.jpg](./img/fpi/tuao2.jpg)
+
+
+
+![shennaichuan.jpg](./img/fpi/shennaichuan.jpg)
+
+
+![pixiang.jpg](./img/fpi/pixiang.jpg)
